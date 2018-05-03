3b63c1e131636601552de120269ba391a3818249
daniel9898 <danyillusion@yahoo.com.ar>
2018-05-03 16:57:51 -0300

    ...

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,37 @@ export class HomePage {
 
   }
 
+    /*foto(campo) {
+    let qua = 80;
+    let tHeight = 400;
+    let tWidth = 400;
+    if (campo.Mejorar_Calidad_de_Foto__c == true) {
+      qua = 90;
+      tHeight = 800;
+      tWidth = 800;
+    }
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        quality: qua,
+        targetHeight: tHeight,
+        targetWidth: tWidth,
+        saveToPhotoAlbum: true,
+        encodingType: this.camera.EncodingType.JPEG
+    }).then((imageData) => {
+      console.log("IMAGE SUCCES FORMULARIO");
+        campo.valor__c = "data:image/jpeg;base64," + imageData;        
+        if (campo.Obtener_Geolocalizaci_n__c) {
+          this.geolocation.getCurrentPosition().then((actualPosition) => {
+            campo.Geolocalizac_n__c = actualPosition.coords.latitude + ', ' + actualPosition.coords.longitude; 
+            this.guardarOk("Guardado",false);
+          }).catch((err)=>{
+            console.log("Error en Foto FORMULARIO :",err);
+            this.guardarOk("Guardado",false);
+          });  
+        }
+    }).catch((err)=>{
+      console.log("Error en Foto:",err);
+    });
+  }*/
+
 }
