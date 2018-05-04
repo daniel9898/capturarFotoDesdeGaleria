3c658eac6181da394b0df4f9d195a6b3350a3962
daniel9898 <danyillusion@yahoo.com.ar>
2018-05-04 15:04:11 -0300

    ...

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,47 +1,83 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
+  image ;
+  opcion ;
+
+  constructor(public navCtrl: NavController, 
+  	          public camera: Camera, 
+  	          public _DomSanitizationService: DomSanitizer,
+              public alertCtrl: AlertController) {}
+
+  options: CameraOptions = {
+    quality: 80,
+    targetHeight: 400,
+    targetWidth: 400,
+    encodingType: this.camera.EncodingType.JPEG
   }
 
-    /*foto(campo) {
-    let qua = 80;
-    let tHeight = 400;
-    let tWidth = 400;
-    if (campo.Mejorar_Calidad_de_Foto__c == true) {
-      qua = 90;
-      tHeight = 800;
-      tWidth = 800;
-    }
-    this.camera.getPicture({
-        destinationType: this.camera.DestinationType.DATA_URL,
-        quality: qua,
-        targetHeight: tHeight,
-        targetWidth: tWidth,
-        saveToPhotoAlbum: true,
-        encodingType: this.camera.EncodingType.JPEG
-    }).then((imageData) => {
-      console.log("IMAGE SUCCES FORMULARIO");
-        campo.valor__c = "data:image/jpeg;base64," + imageData;        
-        if (campo.Obtener_Geolocalizaci_n__c) {
-          this.geolocation.getCurrentPosition().then((actualPosition) => {
-            campo.Geolocalizac_n__c = actualPosition.coords.latitude + ', ' + actualPosition.coords.longitude; 
-            this.guardarOk("Guardado",false);
-          }).catch((err)=>{
-            console.log("Error en Foto FORMULARIO :",err);
-            this.guardarOk("Guardado",false);
-          });  
-        }
-    }).catch((err)=>{
-      console.log("Error en Foto:",err);
+
+  opciones() {
+
+    let alert = this.alertCtrl.create({
+      title: 'Seleccione :',
+      buttons: [
+        {
+          text: 'Imagen nueva', 
+          handler: () => {
+            this.foto('Nueva');
+          }
+        },
+        {
+          text: 'Imagen de galeria',
+          handler: () => {
+            this.foto('Existente');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Buy clicked');
+          }
+        },
+      ]
     });
-  }*/
+    alert.present();
+  }
+
+  foto(opcion : string) {
+
+      let base64Image ;
+      
+      if(opcion == 'Nueva'){
+         delete this.options.sourceType;
+         base64Image = 'data:image/jpeg;base64,';
+         this.options.destinationType = this.camera.DestinationType.DATA_URL;
+      } else{
+         delete this.options.destinationType;
+         base64Image = '';
+         this.options.sourceType = this.camera.PictureSourceType.SAVEDPHOTOALBUM;
+      }
+     
+      this.camera.getPicture(this.options).then((imageData) => {
+
+          base64Image +=  imageData;
+          this.image = this._DomSanitizationService.bypassSecurityTrustUrl(base64Image);   
+      
+      }).catch((err)=>{
+        console.log("Error en Foto:",err);
+      });
+    }
 
 }
